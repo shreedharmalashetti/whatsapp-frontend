096a6d4e65f06f3a65706f3793bafb6395bd189a
shreedharmalashetti <shreedharmalashetti@gmail.com>
2021-12-12 19:50:46 +0530

    ...

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:8000");
+const socket = io("https://socketiowhatsapp.herokuapp.com/");
 
 socket.on("connect", () => {
   console.log("socket io connected");
